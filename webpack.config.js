1a6d0244d8959d4611602c59861c5f8168bcadac
MaxSavostyanov <maxsavostyanov34@gmail.com>
2019-11-07 22:16:52 +0300

    bug fixed: not found background image and fonts

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
 			{
 				test: /\.(png|jpg|gif|ico|svg)$/,
 				use: [
-					'file-loader?name=./img/[name].[ext]',
+					'file-loader?name=/img/[name].[ext]',
 					{
 						loader: 'image-webpack-loader',
 						options: {
@@ -39,7 +39,7 @@ module.exports = {
 			},
 			{
 				test: /\.(eot|ttf|woff|woff2)$/,
-				loader: 'file-loader?name=./vendor/[name].[ext]'
+				loader: 'file-loader?name=/vendor/[name].[ext]'
 			},
 			{
 				test: /\.css$/i,
